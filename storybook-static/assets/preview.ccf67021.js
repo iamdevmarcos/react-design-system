3dae8bb75138e8757d47766cd8db08dffe0e869e
Marcos Andre <marcosdev.me@gmail.com>
2022-10-12 10:03:05 -0300

    add storybook static

---
+++ storybook-static/assets/preview.ccf67021.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.b76751b9.js";import"./iframe.0af88cbb.js";import"./es.map.constructor.100d73d9.js";import"./es.number.to-fixed.0d86f75c.js";import"./index.d9ab9976.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.ccf67021.js.map
